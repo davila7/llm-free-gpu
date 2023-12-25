30a0371236eb3b05a6b88ee05c81ce2f239af1c7
Dani <dan.avila7@gmail.com>
2023-12-24 20:33:28 -0500

    fix

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ torch.backends.cudnn.benchmark = True
 torch.backends.cudnn.deterministic = False
 torch.cuda.empty_cache()
 torch.cuda.set_per_process_memory_fraction(0.5, device=None)
-torch.cuda.set_per_process_memory_growth(True, device=None)
 torch.cuda.set_max_workspace_size(1024 * 1024 * 1024, device=None)
 torch.backends.cuda.reserved_memory.max_split_size_mb = 1024
 
